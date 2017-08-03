84d5837f64f88c3c28e6e65d34cb9a630c2f67cd
Kevin Leutzinger <me@kevinleutzinger.com>
2017-08-02 21:38:15 -0700

    added moves and fixed other stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 var ele = document.createElement("script");
 ele.src = chrome.extension.getURL("/js/tooltip.js");
 document.body.appendChild(ele);
+
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://raw.githubusercontent.com/Zarel/Pokemon-Showdown/master/data/formats-data.js', true);
+xhr.onload = function(e) {
+  console.log(xhr.responseText);
+};
+
+xhr.send();
